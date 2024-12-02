dc498cbbf3c8339720463f9c29a139cc489d1187
Bas van der Hoek <bas@hoek.com>
2024-12-02 10:49:51 +0100

    Log missing remote and changes as warnings

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use clap::{Arg, Command};
+use env_logger::Env;
+use env_logger::Target::Stdout;
 use git2::StatusOptions;
 use git2::{Repository, Status};
+use log::{info, warn};
 
 fn main() {
   let args = Command::new("gitrs").arg(Arg::new("PATH").required(true)).get_matches();
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).target(Stdout).init();
   if !args.args_present() {
     eprintln!("{}", Command::new("my_app").render_help());
     std::process::exit(1);
   }
 
   let filepath = args.get_one::<String>("PATH").unwrap();
-  println!("Opening repository at {}", filepath);
+  info!("Opening repository at {}", filepath);
 
   let repo = match Repository::open(filepath) {
     Ok(repo) => repo,
@@ -19,12 +23,12 @@ fn main() {
 
   let remotes = repo.remotes().unwrap();
   if remotes.len() == 0 {
-    eprintln!("No remotes found");
+    warn!("No remotes found");
   }
   remotes.iter().for_each(|remote| {
     let name = remote.unwrap();
     let remote = repo.find_remote(name).unwrap();
-    println!("  remote {} -> {:?}", name, remote.url());
+    info!("  remote {} -> {:?}", name, remote.url());
   });
 
   repo.branches(None).unwrap().for_each(|branch| {
@@ -32,9 +36,9 @@ fn main() {
     let name = branch.name().unwrap().unwrap();
     if let Ok(upstream) = branch.upstream() {
       let upstream_name = upstream.name().unwrap().unwrap();
-      println!("upstream {} -> {}", name, upstream_name);
+      info!("upstream {} -> {}", name, upstream_name);
     } else {
-      println!("  branch {}", name);
+      warn!("  branch {}", name);
     }
   });
 
@@ -52,7 +56,7 @@ fn main() {
       Status::WT_NEW | Status::INDEX_NEW => untracked += 1,
       _ => unspecified += 1,
     }
-    println!("{:?}: {}", entry.status(), path);
+    warn!("{:?}: {}", entry.status(), path);
   });
-  println!("{} modified, {} deleted, {} untracked, {} unspecified\n", modified, deleted, untracked, unspecified);
+  info!("{} modified, {} deleted, {} untracked, {} unspecified\n", modified, deleted, untracked, unspecified);
 }
